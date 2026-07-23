0c2bc8fd6b6fa83a78218e1f92bf300ba04d94a1
Aaditya Bhatnagar <11aadityab@gmail.com>
2026-07-23 22:13:41 +0400

    problem A edited

--- cf/Round_1110_div1+2/a.cpp
+++ cf/Round_1110_div1+2/a.cpp
@@ -1,34 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 int main() {
-    int t;
-    std::cin >> t;
-    
-    while(t--) {
-        int n, k;
-        std::string s;
-        std::cin >> n >> k;
-        std::cin >> s;
-        int count = 0;
+  int t;
+  std::cin >> t;
 
-        if (2 * k > n) {
-            std::cout << -1 << std::endl;
-            continue;
-        }
+  while (t--) {
+    int n, k;
+    std::string s;
+    std::cin >> n >> k;
+    std::cin >> s;
+    int count = 0;
 
-        for (int i = 0; i < k; i++) {
-            if (s[i] == 'L') {
-                count++;
-            }
-        }
-
-        for (int i = n - 1; i > n - k - 1; i--) {
-            if (s[i] == 'R') {
-                count++;
-            }
-        }
-
-        std::cout << count << std::endl;
+    for (int i = 0; i < k; i++) {
+      count += (s[i] == 'L');
+      count += (s[n - 1 - i] == 'R');
     }
-    return 0;
+
+    std::cout << count << std::endl;
+  }
+  return 0;
 }
